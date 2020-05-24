593a03ab8cf33721d462056d3e5a956557d6f5a7
Jaime López <loama18@gmail.com>
2020-05-23 19:30:34 -0500

    init vue app and add api request

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,33 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    hola
+    {{days}}
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import axios from 'axios'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  data () {
+    return {
+      days: []
     }
+  },
+  mounted () {
+    const self = this
+    axios({
+      method: 'get',
+      url: 'https://car-insurance-test.herokuapp.com/'
+    })
+      .then((response) => {
+        console.log(response)
+        self.days = response.data
+      })
   }
 }
+</script>
+
+<style lang="sass">
+#app
 </style>
